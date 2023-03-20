f4acd184eac114ba3255043315f666644a38d730
carlos <117171467+N3XUof@users.noreply.github.com>
2023-03-20 16:44:29 -0300

    Update Principal.py

--- Principal.py
+++ Principal.py
@@ -1,5 +1,6 @@
-pessoas = int(input('quantidade de pessoas: '))
-bebida = int(input('quantidade de litros: '))
-resultado = pessoas*bebida
-
-print('resultado {} e {} = {}'.format(pessoas, bebida, resultado))
+print('Saiba quantos litros você precisará para uma festa!)
+      pessoas = int(input('quantidade de pessoas: '))
+      bebida = int(input('quantidade de litros: '))
+      resultado = pessoas*bebida
+      
+      print('resultado {} e {} = {}'.format(pessoas, bebida, resultado))
