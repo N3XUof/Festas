aecabc34d960cd90c95c359b0f153f770a22616a
carlos <117171467+N3XUof@users.noreply.github.com>
2023-03-20 16:42:59 -0300

    Add files via upload

---
+++ Principal.py
@@ -0,0 +1,5 @@
+pessoas = int(input('quantidade de pessoas: '))
+bebida = int(input('quantidade de litros: '))
+resultado = pessoas*bebida
+
+print('resultado {} e {} = {}'.format(pessoas, bebida, resultado))
